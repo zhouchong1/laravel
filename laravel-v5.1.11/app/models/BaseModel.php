4647ce5c42b7589c10b81cee9d83acdab7acea2a
zhouchong1 <368764249@qq.com>
2015-12-10 19:17:47 +0800

    test

---
+++ laravel-v5.1.11/app/models/BaseModel.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2015/12/9
+ * Time: 23:27
+ */
+
+namespace App\models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class BaseModel extends  Model{
+
+}
---
+++ laravel-v5.1.11/app/models/TestModel.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2015/12/9
+ * Time: 23:40
+ */
+namespace App\models;
+class TestModel extends  \App\models\BaseModel{
+
+}
